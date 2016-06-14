db93d8e81a00806bf037ec814d27047bfffc81d3
Bernard Kanyolo <bernard.kanyolo@gmail.com>
2016-06-15 01:47:50 +0300

    Complete NotesApplication class definition

--- OOP assignment/noteapp.py
+++ OOP assignment/noteapp.py
@@ -1,13 +1,44 @@
 class NotesApplication(object):
-	'''note taking application class definition'''
+    '''note taking application class definition'''
 
-	def __init__(self, author):
-		'''constructor for the Notes Application. 
-		Takes a string, the author, and initialises a list to
-		store all the different notes''' 
-		self.author = author
+    def __init__(self, author):
+        '''constructor for the Notes Application.
+        Takes a string, the author, and initialises a list to
+        store all the different notes'''
+        self.author = author
+        self.notes = []
 
-		self.notes = []
+    def create(self, note_content):
+        '''This function takes the note content as the
+         parameter and adds it to the notes list of the object.
+         '''
+        self.notes.append(note_content)
 
+    def list(self):
+        '''This function lists out each of the notes
+        in the notes list'''
+        for index, note in enumerate(self.notes):
+            args = (index, note, self.author)
+            print "Note ID: {0}\n{1}\n\nBy Author {2}\n\n".format(args)
 
-	
+    def get(self, note_id):
+        '''This function takes a note_id which
+        refers to the index of the note in the notes
+        list and returns the content of that note as a string
+        '''
+        return self.notes[note_id]
+
+    def search(self, search_text):
+        '''This function takes a search string search
+        text and prints all the notes with that text
+        '''
+        for index, note in enumerate(self.notes):
+            if search_text in note:
+                args = (search_text, index, note, self.author)
+                print "Showing results for search '{0}'\n\nNote ID: {1}\n{2}\n\nBy Author {3}\n\n".format(args)
+
+    def edit(self, note_id, new_content):
+        '''This function replaces the content in the
+        note at note_id with new_content
+        '''
+        self.notes[note_id] = new_content
---
+++ OOP assignment/noteapptests.py
@@ -0,0 +1,27 @@
+import unittest
+from noteapp import NotesApplication
+
+
+class TestNoteAppClass(unittest.TestCase):
+
+    def setUp(self):
+        self.note_app = NotesApplication("Ben")
+
+    def test_init(self):
+        self.assertEqual(self.note_app.author, 'Ben',
+                         msg="author attribute is incorrect")
+        self.assertEqual(self.note_app.notes, [],
+                         msg="notes attribute is incorrect")
+
+    def test_create(self):
+        self.note_app.create("Note 0")
+        self.assertEqual(len(self.note_app.notes), 1,
+                         msg="notes not added correctly")
+
+    def test_list(self):
+        sample_out = "Note ID: 0\nNote 0\n\nBy Author Ben K\n"
+        self.assertEqual(self.note_app.list(), sample_out,
+                         msg="notes not listed correctly")
+
+if __name__ == '__main__':
+    unittest.main()
