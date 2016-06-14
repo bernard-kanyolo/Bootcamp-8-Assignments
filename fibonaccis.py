5236b112f85c21c406cc48c4942393b892832b60
Bernard Kanyolo <bernard.kanyolo@gmail.com>
2016-06-14 23:51:33 +0300

    create working implementation

--- fibonaccis.py
+++ fibonaccis.py
@@ -1,20 +1,17 @@
 def nth_fib(num):
-	if num < 1:
-		return -1
-	elif num == 1:
-		return 0
-	elif num == 2:
-		return 1
+    if num < 1:
+        return -1
+    elif num == 1:
+        return 0
+    elif num == 2:
+        return 1
 
-	fibs = [0,1]
-	num-=2
-	last_index = len(fibs) - 1
+    fibs = [0, 1]
+    num -= 2
+    last_index = len(fibs) - 1
 
-	while num:
-		fibs.append(fibs[last_index] + fibs[last_index - 1])
-		last_index, num = last_index + 1, num - 1
+    while num:
+        fibs.append(fibs[last_index] + fibs[last_index - 1])
+        last_index, num = last_index + 1, num - 1
 
-	print fibs
-	return fibs[last_index]
-
-print nth_fib(100)
+    return fibs[last_index]
