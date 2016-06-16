3b95b667297237c40a35b9414c4887291a835145
Bernard Kanyolo <bernard.kanyolo@gmail.com>
2016-06-16 05:34:20 +0300

    Factor out test cases to their own files

--- Programming Logic/fibonaccis.py
+++ Programming Logic/fibonaccis.py
@@ -1,5 +1,3 @@
-import unittest
-
 
 def nth_fib(num):
     '''takes a number, num, greater than 0 and returns the numth fibonacci
@@ -35,28 +33,3 @@ def nth_fib(num):
 
     # the last fibonacci term in our list is the answer
     return fibs[last_index]
-
-
-class NthFibonacciTestCases(unittest.TestCase):
-
-    def test_lower_edge(self):
-        self.assertEqual(nth_fib(0), 'invalid input',
-                         msg='An input of zero should return an error message')
-        self.assertEqual(nth_fib(-1), 'invalid input',
-                         msg='An input < 0 should return an error message')
-
-    def test_normal_behaviour(self):
-        self.assertEqual(nth_fib(1), 0, msg='The first fib number is 0')
-        self.assertEqual(nth_fib(2), 1,
-                         msg='The second fib number is 1')
-        self.assertEqual(nth_fib(10), 34,
-                         msg='The tenth fib number is 34')
-
-    def test_invalid_input(self):
-        self.assertEqual(nth_fib(""), 'invalid input',
-                         msg='A string should return an error message')
-        self.assertEqual(nth_fib({}), 'invalid input',
-                         msg='A dictionary should return an error message')
-
-if __name__ == '__main__':
-    unittest.main()
--- Programming Logic/primes.py
+++ Programming Logic/primes.py
@@ -1,5 +1,3 @@
-import unittest
-
 
 def gen_primes(num):
     '''takes a number, num, and returns a list of the first n prime numbers
@@ -31,28 +29,3 @@ def gen_primes(num):
         candidate += 2
 
     return primes
-
-
-class GeneratePrimesTestCases(unittest.TestCase):
-
-    def test_lower_edge(self):
-        self.assertEqual(gen_primes(0), [],
-                         msg='An input of zero should yield an empty list')
-        self.assertEqual(gen_primes(-1), [],
-                         msg='An input less than 0 should yield an empty list')
-
-    def test_normal_behaviour(self):
-        self.assertEqual(gen_primes(1), [2], msg='The first prime number is 2')
-        self.assertEqual(gen_primes(2), [2, 3],
-                         msg='The first 2 prime numbers are 2 and 3')
-        self.assertEqual(gen_primes(10), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29],
-                         msg='Incorrect list of first 10 prime numbers')
-
-    def test_invalid_input(self):
-        self.assertEqual(gen_primes(""), [],
-                         msg='A string should return the empty list')
-        self.assertEqual(gen_primes({}), [],
-                         msg='A dictionary should return the empty list')
-
-if __name__ == '__main__':
-    unittest.main()
