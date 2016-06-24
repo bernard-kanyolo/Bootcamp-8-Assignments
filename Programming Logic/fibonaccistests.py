a4a131bd9d5d21dbbf9933b6b75f1ffa7f5d3e67
Bernard Kanyolo <bernard.kanyolo@gmail.com>
2016-06-24 16:03:24 +0300

    Refractor some tests

--- Programming Logic/fibonaccistests.py
+++ Programming Logic/fibonaccistests.py
@@ -6,35 +6,57 @@ class NthFibonacciTestCases(unittest.TestCase):
     """unit tests for fibonacci numbers from 0 to n
     """
 
-    def test_lower_edge(self):
-        """test lower edge case
+    def test_lower_edge_less_than_0(self):
+        """test lower edge case < 0
         """
-
         self.assertListEqual(fib_to(-1), [],
                              msg='An input < 0 should return the empty list')
+
+    def test_lower_edge_zero(self):
+        """test lower edge of 0
+        """
         self.assertListEqual(fib_to(0), [],
                              msg='An input of 0 should return the empty list')
 
-    def test_normal_behaviour(self):
-        """test a few random values for correctness
+    def test_fib_one_is_zero(self):
+        """test normal functionality fib 1
         """
-
         self.assertListEqual(fib_to(1), [0],
                              msg='The first fib number is 0')
+
+    def test_normal_functionality_2(self):
+        """test for fib_to 2
+        """
         self.assertListEqual(fib_to(2), [0, 1],
                              msg='The first two fib numbers are 0,1')
+
+    def test_normal_functionality_10(self):
+        """test for fib_to 10
+        """
         self.assertListEqual(fib_to(10), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34],
                              msg='The first 10 fib numbers are incorrect')
+
+    def test_normal_functionality_7(self):
+        """test for fib_to 7
+        """
         self.assertListEqual(fib_to(7), [0, 1, 1, 2, 3, 5, 8],
                              msg='The first 7 fib numbers are incorrect')
 
-    def test_invalid_input(self):
-        """test that input is properly validated
+    def test_invalid_input_string(self):
+        """test that string input isn't accepted
         """
         self.assertEqual(fib_to(""), 'invalid input',
                          msg='A string should return an error message')
+
+    def test_invalid_input_dict(self):
+        """test that dict input isn't accepted
+        """
         self.assertEqual(fib_to({}), 'invalid input',
                          msg='A dictionary should return an error message')
+
+    def test_invalid_input_list(self):
+        """test that list input isn't accepted
+        """
         self.assertEqual(fib_to([]), 'invalid input',
                          msg='A list should return an error message')
 
